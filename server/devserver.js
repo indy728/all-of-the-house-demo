748d45214f8279703a5bbcb967070abd7064705d
Kyle Murray <kdevlinmurray@gmail.com>
2020-07-30 15:10:08 -0700

    linting issues with react-scripts, react client up and running

--- server/devserver.js
+++ server/devserver.js
@@ -1,3 +1,13 @@
 require('dotenv').config();
-const app = require('express')
-const app
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 4350;
+
+app.listen(PORT, () => console.log(`devserver is up and running on port ${PORT}`));
